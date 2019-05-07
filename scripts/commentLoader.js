325745470f5704b1ba863da619c0240ac134de25
npalomba <nicola.palomba@studenti.galileilivorno.gov.it>
2019-05-07 10:21:05 +0200

    Signed-off-by: npalomba <nicola.palomba@studenti.galileilivorno.gov.it>

--- scripts/commentLoader.js
+++ scripts/commentLoader.js
@@ -3,6 +3,8 @@ function loadComments() {
 
     $.ajax({
         url: "https://github.com/npalomba/ascent-devlog/tree/master/_data/comments/%7B%7B%20page.slug%20%7D%7D",
+        crossDomain: true,
+        dataType: "jsonp",
         success: function(data){
             $(data).find("a:contains(.yml)").each(function(){
                 // will loop through
