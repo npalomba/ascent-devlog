db5cbff13f20eca36ec878735caf423b833254ec
npalomba <nicola.palomba@studenti.galileilivorno.gov.it>
2019-05-07 22:54:14 +0200

    Almost finished implementation of dynamically loaded comments.
    Signed-off-by: npalomba <nicola.palomba@studenti.galileilivorno.gov.it>

--- scripts/commentLoader.js
+++ scripts/commentLoader.js
@@ -2,17 +2,60 @@ let commentsDirectory = "https://api.github.com/repos/npalomba/ascent-devlog/con
 
 function loadComments() {
     let threadNumber = document.head.querySelector("[property~=commentThread][content]").content;
+    threadNumber = threadNumber.replace(/'/g, "");
     let jsonStuff = httpGet(commentsDirectory);
+    let commentContainer = document.getElementById("existingComments");
 
     jsonStuff = JSON.parse(jsonStuff);
+
+    if (jsonStuff.length > 0) {
+        let h2 = document.createElement("h2");
+        h2.textContent = "Comments";
+        commentContainer.appendChild(h2);
+    }
 
     for (let i=0; i<jsonStuff.length; i++) {
         let object = jsonStuff[i];
         let staticManData = JSON.parse(httpGet(object["url"]));
         staticManData = atob(staticManData["content"]);
 
-        let message = getStaticmanField(staticManData, "message:");
-        let name = getStaticmanField(staticManData, "name:");
+        let commentThreadNumber = getStaticmanField(staticManData, "thread:");
+        commentThreadNumber = commentThreadNumber.replace(/'/g, "");
+
+        alert(Number.parseInt(commentThreadNumber) + "," + Number.parseInt(threadNumber));
+
+        if (Number.parseInt(commentThreadNumber) === Number.parseInt(threadNumber)) {
+            let message = getStaticmanField(staticManData, "message:");
+            let name = getStaticmanField(staticManData, "name:");
+            let date = getStaticmanField(staticManData, "date:");
+
+            let root = document.createElement("div");
+            root.setAttribute("class", "comment");
+
+            let commentName = document.createElement("span");
+            let curdate = new Date(null);
+            curdate.setTime(Number.parseInt(date) * 1000);
+
+            commentName.setAttribute("class", "commentName");
+            commentName.textContent = name + " commented on:";
+
+            let commentDate = document.createElement("span");
+            commentDate.setAttribute("class", "commentDate");
+            commentDate.textContent =  curdate + ":";
+
+            let commentContent = document.createElement("div");
+            commentContent.setAttribute("class", "commentBody");
+            commentContent.textContent = message;
+
+            root.appendChild(commentName);
+            root.appendChild(commentDate);
+            root.appendChild(commentContent);
+
+            commentContainer.appendChild(root);
+        }
+        else {
+            alert("oh no");
+        }
         /** TODO: scommenta questo
          *
          */
@@ -38,5 +81,21 @@ function getStaticmanField(message, field) {
             return ret;
         }
     }
-
 }
+
+/**
+ <h2>Comments</h2>
+ <div class = "comment">
+ <span class = "commentName">Gianni commented on: </span>
+ <span class = "commentDate">05/04/2000</span>
+ <br><br>
+ <div class = "commentBody">I think this is the normal comment length. Will you implement other stuff?</div>
+ </div>
+
+ <div class = "comment">
+ <span class = "commentName">Gianni commented on: </span>
+ <span class = "commentDate">05/04/2000</span>
+ <br><br>
+ <div class = "commentBody">I think this is the normal comment length. Will you implement other stuff?</div>
+ </div>
+ */
